418da8acdeea42d176b402d849fcc992495a2ba7
lenttse60623@fpt.edu.vn <lenttse60623@fpt.edu.vn@120f915d-c8e0-3b31-d421-bc3191cc275f>
2014-06-17 11:18:15 +0000

    commit ImportExcel

---
+++ trunk/stuff/lentt/ImportExcel/ImportExcel/Models/ProductMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImportExcel.Models
+{
+    public class ProductMap
+    {
+        public String stt { set; get; }
+        public String ten { set; get; }
+        public String trongso { set; get; }
+        public String loai { set; get; }
+        public ProductMap()
+        {
+
+        }   
+    }
+}
