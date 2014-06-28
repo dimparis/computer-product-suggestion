4c739373ec56bbd92a4f03fccb2b031ba607ef35
lenttse60623@fpt.edu.vn <lenttse60623@fpt.edu.vn@120f915d-c8e0-3b31-d421-bc3191cc275f>
2014-06-28 17:00:35 +0000

    Update ImportexcelLap

---
+++ trunk/code/CPS_Solution/CPS_Solution/Areas/Admin/Models/LapData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CPS_Solution.Areas.Admin.Models
+{
+    public class LapData
+    {
+        public string stt { get; set; }
+        public string Name { get; set; }
+        public string Imagelink { get; set; }
+        public string CPU { get; set; }
+        public string VGA { get; set; }
+        public string HDD { get; set; }
+        public string Display { get; set; }
+        public string RAM { get; set; }
+    }
+}
