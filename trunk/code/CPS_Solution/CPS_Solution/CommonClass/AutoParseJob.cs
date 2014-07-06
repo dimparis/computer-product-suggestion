35a095786d6fc24a44b111863f1d3a16efc7adf4
iv250509@gmail.com <iv250509@gmail.com@120f915d-c8e0-3b31-d421-bc3191cc275f>
2014-07-06 17:50:00 +0000

    add for auto parse

--- trunk/code/CPS_Solution/CPS_Solution/CommonClass/AutoParseJob.cs
+++ trunk/code/CPS_Solution/CPS_Solution/CommonClass/AutoParseJob.cs
@@ -20,70 +20,80 @@ namespace CPS_Solution.CommonClass
             Regex reg = new Regex(patter);
             var parseInfoes = databaseContext.ParseInfoes.Where(x => x.IsActive == true).OrderBy(x => x.Parselink).ToList();
             var rcmdProduct = databaseContext.RecommendProducts.Where(x => x.IsApprove == false).OrderBy(x => x.Parselink).ToList();
+            Task.Factory.StartNew(() => DoTask(rcmdProduct, parseInfoes));
+        }
+        public void DoTask(List<RecommendProduct> listOfRecommend, List<ParseInfo> listOfParseInfo)
+        {
+            string patter = "://|/";
+            Regex reg = new Regex(patter);
             List<ParseInfo> listInfo = new List<ParseInfo>();
             ParseInfo infoItem = null;
             List<RecommendProduct> list = new List<RecommendProduct>();
-            foreach (var info in parseInfoes)
+            if (listOfRecommend != null)
             {
-                infoItem = new ParseInfo
+                foreach (var info in listOfParseInfo)
                 {
-                    CPUXPath = info.CPUXPath,
-                    DisplayXPath = info.DisplayXPath,
-                    HDDXPath = info.HDDXPath,
-                    ImageXpath = info.ImageXpath,
-                    IsActive = true,
-                    Name = info.Name,
-                    Parselink = info.Parselink,
-                    RAMXPath = info.RAMXPath,
-                    VGAXPath = info.VGAXPath,
-                };
-                listInfo.Add(infoItem);
-
-            }
-            var fitlerList = listInfo.Select(x => new
-            {
-                host = reg.Split(x.Parselink)[1],
-                x.CPUXPath,
-                x.DisplayXPath,
-                x.HDDXPath,
-                x.ImageXpath,
-                x.Name,
-                x.Parselink,
-                x.RAMXPath,
-                x.VGAXPath,
-                x.IsActive
-            })
-                .Distinct()
-                .GroupBy(y => new { y.host })
-                .ToArray();
-            ProductParserCreator model = null;
-            foreach (var item in fitlerList)
-            {
-                foreach (var recommend in rcmdProduct)
-                {
-                    if (recommend.Parselink.Contains(item.Key.host))
+                    infoItem = new ParseInfo
                     {
-                        model = new ProductParserCreator
+                        CPUXPath = info.CPUXPath,
+                        DisplayXPath = info.DisplayXPath,
+                        HDDXPath = info.HDDXPath,
+                        ImageXpath = info.ImageXpath,
+                        IsActive = true,
+                        Name = info.Name,
+                        Parselink = info.Parselink,
+                        RAMXPath = info.RAMXPath,
+                        VGAXPath = info.VGAXPath,
+                    };
+                    listInfo.Add(infoItem);
+
+                }
+                var fitlerList = listInfo.Select(x => new
+                {
+                    host = reg.Split(x.Parselink)[1],
+                    x.CPUXPath,
+                    x.DisplayXPath,
+                    x.HDDXPath,
+                    x.ImageXpath,
+                    x.Name,
+                    x.Parselink,
+                    x.RAMXPath,
+                    x.VGAXPath,
+                    x.IsActive
+                })
+                    .Distinct()
+                    .GroupBy(y => new { y.host })
+                    .ToArray();
+                ProductParserCreator model = null;
+                foreach (var item in fitlerList)
+                {
+                    foreach (var recommend in listOfRecommend)
+                    {
+                        if (recommend.Parselink.Contains(item.Key.host))
                         {
-                            CPUXpath = item.First().CPUXPath,
-                            DisplayXpath = item.First().DisplayXPath,
-                            HDDXpath = item.First().HDDXPath,
-                            ImageXpath = item.First().ImageXpath,
-                            IsActive = true,
-                            ParseProductLink = item.First().Parselink,
-                            ProductNameXpath = item.First().Name,
-                            RAMXpath = item.First().RAMXPath,
-                            VGAXpath = item.First().VGAXPath,
-                            RecommendProductId = recommend.ID.ToString(),
-                        };
-                        Task.Factory.StartNew(() => ParserHelper.ParseProductData(model));
-                        int rcmId = Int32.Parse(model.RecommendProductId);
-                        var recommendProduct = databaseContext.RecommendProducts.Where(x => x.ID == rcmId).FirstOrDefault();
-                        recommendProduct.IsApprove = true;
+                            model = new ProductParserCreator
+                            {
+                                CPUXpath = item.First().CPUXPath,
+                                DisplayXpath = item.First().DisplayXPath,
+                                HDDXpath = item.First().HDDXPath,
+                                ImageXpath = item.First().ImageXpath,
+                                IsActive = true,
+                                ParseProductLink = item.First().Parselink,
+                                ProductNameXpath = item.First().Name,
+                                RAMXpath = item.First().RAMXPath,
+                                VGAXpath = item.First().VGAXPath,
+                                RecommendProductId = recommend.ID.ToString(),
+                            };
+                            ParserHelper.ParseProductData(model);
+                            int rcmId = Int32.Parse(model.RecommendProductId);
+                            var recommendProduct = databaseContext.RecommendProducts.Where(x => x.ID == rcmId).FirstOrDefault();
+                            recommendProduct.IsApprove = true;
+                        }
                     }
                 }
+                databaseContext.SaveChanges();
             }
-            databaseContext.SaveChanges();
+
         }
     }
 }
