d7596b2b0fc1398eef51fce091561e00de84b113
iv250509 <iv250509@gmail.com>
2014-07-13 11:12:07 +0000

    modifeid for new db

--- code/CPS_Solution/CPS_Solution/Areas/Admin/Controllers/RecommendController.cs
+++ code/CPS_Solution/CPS_Solution/Areas/Admin/Controllers/RecommendController.cs
@@ -17,7 +17,7 @@ namespace CPS_Solution.Areas.Admin.Controllers
         private CPS_SolutionEntities context = new CPS_SolutionEntities();
         public ActionResult Index()
         {
-            var recommendProduct = context.RecommendProducts.Where(x => x.IsApprove ==null);
+            var recommendProduct = context.RecommendProducts.Where(x => x.IsApprove == null);
 
             return View(recommendProduct);
         }
@@ -46,6 +46,7 @@ namespace CPS_Solution.Areas.Admin.Controllers
             var productInfo = new ParseInfo
             {
                 Name = model.ProductNameXpath,
+                PriceXPath = model.PriceXpath,
                 ImageXpath = model.ImageXpath,
                 Parselink = model.ParseProductLink,
                 CPUXPath = model.CPUXpath,
@@ -59,13 +60,30 @@ namespace CPS_Solution.Areas.Admin.Controllers
             var uri = new Uri(model.ParseProductLink);
             string host = uri.GetLeftPart(UriPartial.Authority);
             var parseInfo = context.ParseInfoes.FirstOrDefault(info => info.Parselink.Contains(host));
-            if (parseInfo == null) 
+            if (parseInfo == null)
             {
+                //Create parser if not exist
                 context.ParseInfoes.Add(productInfo);
-                context.SaveChanges();  
-            }           
+                context.SaveChanges();
+            }
+            else
+            {
+                //If exist Update Parser
+                var newParseInfo = context.ParseInfoes.Where(x => x.ID == parseInfo.ID).FirstOrDefault();
+                newParseInfo.Name = model.ProductNameXpath;
+                newParseInfo.PriceXPath = model.PriceXpath;
+                newParseInfo.ImageXpath = model.ImageXpath;
+                newParseInfo.Parselink = model.ParseProductLink;
+                newParseInfo.CPUXPath = model.CPUXpath;
+                newParseInfo.DisplayXPath = model.DisplayXpath;
+                newParseInfo.HDDXPath = model.HDDXpath;
+                newParseInfo.RAMXPath = model.RAMXpath;
+                newParseInfo.VGAXPath = model.VGAXpath;
+                newParseInfo.IsActive = true;
+                context.SaveChanges();
+            }
             Task.Factory.StartNew(() => ParserHelper.ParseProductData(model));
-            int  rcmId= Int32.Parse(model.RecommendProductId);
+            int rcmId = Int32.Parse(model.RecommendProductId);
             var recommendProduct = context.RecommendProducts.Where(x => x.ID == rcmId).FirstOrDefault();
             recommendProduct.IsApprove = true;
             context.SaveChanges();
@@ -88,7 +106,7 @@ namespace CPS_Solution.Areas.Admin.Controllers
                 {
                     int i = Int32.Parse(id);
                     var recommendProduct = context.RecommendProducts.Where(x => x.ID == i).FirstOrDefault();
-                    recommendProduct.IsApprove = false;       
+                    recommendProduct.IsApprove = false;
                 }
                 context.SaveChanges();
                 check = true;
---
+++ code/CPS_Solution/CPS_Solution/Areas/Admin/Helpers/PriceHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CPS_Solution.Areas.Admin.Helpers
+{
+    public class PriceHelper
+    {
+        public static double ConvertPrice(string price)
+        {
+            string result = "";
+            int count = 0;
+            foreach (char c in price)
+            {
+                if (Char.IsDigit(c))
+                {
+                    result += c;
+                    count++;
+                }
+                if (!Char.IsDigit(c) && count > 1 && !Char.IsWhiteSpace(c) && c != '.' && c != ',')
+                {
+                    break;
+                }
+            }
+            double tmp = Double.Parse(result);
+            return tmp;
+        }
+    }
+}
