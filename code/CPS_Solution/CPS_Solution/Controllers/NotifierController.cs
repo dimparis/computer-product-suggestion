9195634d54ed0ea24665bc12208e3853cad224b0
iv250509 <iv250509@gmail.com>
2014-08-06 20:51:09 +0000

    modified for notification

--- code/CPS_Solution/CPS_Solution/Controllers/NotifierController.cs
+++ code/CPS_Solution/CPS_Solution/Controllers/NotifierController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CPS_Solution.EntityFramework;
+using CPS_Solution.CommonClass;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
 namespace CPS_Solution.Controllers
 {
     public class NotifierController : Controller
@@ -23,24 +27,54 @@ namespace CPS_Solution.Controllers
             if (account != null)
             {
                 var approveRecommendProduct = context.RecommendProducts.Where(x => x.Username == username && x.IsSeen == false && x.IsApprove == true).ToList();
-                List<RecommendProduct> data = new List<RecommendProduct>();
-
+                List<AliasProduct> products = new List<AliasProduct>();
                 foreach (var a in approveRecommendProduct)
                 {
-                    data.Add(a);
-                        a.IsSeen = true;
-                }
-                context.SaveChanges();
-                if (data.Count > 0)
-                {
-                    List<AliasProduct> products = new List<AliasProduct>();
-                    foreach (var item in data) 
+                    if (approveRecommendProduct.Count > 0)
                     {
-                        var newItem = context.AliasProducts.Where(x => x.URL.Contains(item.Parselink)).FirstOrDefault();
-                        products.Add(newItem);
+                        foreach (var item in approveRecommendProduct)
+                        {
+                            var newItem = context.AliasProducts.Where(x => x.URL.Contains(item.Parselink) && x.IsActive == true && x.IsMain == true).FirstOrDefault();
+                            var newProductAttribute = context.ProductAttributes.Where(x => x.ProductID == newItem.ProductID).ToList();
+                            if (newItem != null)
+                            {
+                                if (newProductAttribute.Count() >= 5)
+                                {
+                                    products.Add(newItem);
+                                    a.IsSeen = true;
+                                    context.SaveChanges();
+                                }
+                            }
+                        }
+                        AutoSendMail sendMail = new AutoSendMail();
+                        Task.Factory.StartNew(() => sendMail.AutoSendMailforUser(approveRecommendProduct));
+                        return Json(products.Select(product => new { name = product.Name, id = product.ProductID }).ToList(), JsonRequestBehavior.AllowGet);
                     }
-                    return Json(products.Select(product => new { name = product.Name ,parseLink = product.URL }).ToList(), JsonRequestBehavior.AllowGet);
                 }
+
+            }
+            return Json("NoneData", JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult LoadNotification(string username)
+        {
+
+            var account = context.Accounts.Select(x => new { x.Username }).Where(x => x.Username == username).FirstOrDefault();
+            if (account != null)
+            {
+                var approveRecommendProduct = context.RecommendProducts.Where(x => x.Username == username && x.IsSeen == true && x.IsApprove == true).OrderByDescending(x => x.RecommendTime).ToList();
+                List<AliasProduct> products = new List<AliasProduct>();
+                foreach (var item in approveRecommendProduct)
+                {
+                    if (approveRecommendProduct.Count > 0)
+                    {
+                        var newItem = context.AliasProducts.Where(x => x.URL.Contains(item.Parselink) && x.IsActive == true && x.IsMain == true).FirstOrDefault();
+                        if (newItem != null)
+                        {
+                            products.Add(newItem);
+                        }
+                    }
+                }
+                return Json(products.Select(product => new { name = product.Name, id = product.ProductID }).ToList(), JsonRequestBehavior.AllowGet);
             }
             return Json("NoneData", JsonRequestBehavior.AllowGet);
         }
